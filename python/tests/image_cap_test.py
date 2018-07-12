0cd50a30dcb7d47a0e9545d25fe55393fae74ad6
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-07-12 06:37:22 -0500

    Adding image capture for interval test #1

---
+++ python/tests/image_cap_test.py
@@ -0,0 +1,55 @@
+import os
+import pyscreenshot as ImageGrab
+import unittest
+from datetime import datetime
+
+
+class TestScreenshots(unittest.TestCase):
+
+    # Test methods
+
+    def test_take_x_screenshots(self):
+        im_folder_path = 'image_cap_images'
+        # Helpers
+
+        def folder_item_count():
+            files = os.listdir(im_folder_path)
+            return len(files)
+
+        def do_cleanup():
+            im_list = os.listdir(im_folder_path)
+
+            for im_file in im_list:
+                im_file_path = os.path.join(im_folder_path, im_file)
+                try:
+                    if os.path.isfile(im_file_path):
+                        os.unlink(im_file_path)
+                except Exception as e:
+                    print(e)
+
+        # Will need to create an object that will handle starting and stopping of this interval process
+
+        def do_capture_loop(trigger):
+            while not folder_item_count() == 5:
+                trigger()
+
+            do_cleanup()
+            self.assertTrue(True)
+
+        def take_screenshot():
+            # grab screenshot
+            im = ImageGrab.grab()
+            datetime_str = str(datetime.now())
+
+            # Save screenshot and add dir
+            im.save(f'{im_folder_path}/{datetime_str}.png')
+            print('Added screenshot to folder')
+
+        do_capture_loop(take_screenshot)
+
+
+# MAD PROPS DontCallMeLateForDinner
+if __name__ == '__main__':
+    unittest.main()
+else:
+    print('Not main, not running tests.')
